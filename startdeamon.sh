76374cba76f133a673a075e706ba8f237a0675e6
pennybreak <ryan.pennebaker@gmail.com>
2014-06-22 20:23:33 -0500

    Update startdeamon.sh

--- startdeamon.sh
+++ startdeamon.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Clone Peershares repo and compile deamon
 cd ~/
+echo "Cloning repo ..."
 git clone https://github.com/peershares/peershares.git
 cd peershares/src/
+echo "Compiling source ..."
 make -f makefile.unix BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.4.8/include" BDB_LIB_PATH="/usr/local/BerkeleyDB.4.8/lib"
-./peersharesd &
-sleep 5
+echo "Starting deamon ..."
+./peersharesd
+sleep 10
 watch -n 30 ./peersharesd getinfo
